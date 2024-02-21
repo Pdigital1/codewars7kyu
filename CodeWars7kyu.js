06b7314ace1ba9d8e6994a8bfb5e78285efb9cbc
Bimbola Omishore <bomishore85@gmail.com>
2024-02-21 11:35:17 -0500

    eigth commit

--- CodeWars7kyu.js
+++ CodeWars7kyu.js
@@ -130,3 +130,17 @@ function incrementer(nums) {
   return nums == [] ? [] : nums.map((x,i)=> (x+(i+1))%10)
   //mod 10 will always give you a the last number of a number > than 2 digits 
 }
+
+// Currying functions: multiply all elements in an array
+// To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+
+// The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+// Example:
+
+// multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+// You must not mutate the original array.
+
+// Here's a nice Youtube video about currying, which might help you if this is new to you.
+
+const multiplyAll = arr => n => arr.map(x => x*n)
