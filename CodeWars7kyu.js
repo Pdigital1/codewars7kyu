49e98879bdbf63223bf8bb1e9b0890583cf5eaba
Bimbola Omishore <bomishore85@gmail.com>
2024-02-22 11:59:50 -0500

    ninth commit

--- CodeWars7kyu.js
+++ CodeWars7kyu.js
@@ -144,3 +144,24 @@ function incrementer(nums) {
 // Here's a nice Youtube video about currying, which might help you if this is new to you.
 
 const multiplyAll = arr => n => arr.map(x => x*n)
+// DESCRIPTION:
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4.
+
+
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(''),2)
+};
