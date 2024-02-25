3f0727da268985b2b6f0b7c5b4a381a63481a429
Bimbola Omishore <bomishore85@gmail.com>
2024-02-25 14:38:05 -0500

    twelfth commit

--- CodeWars7kyu.js
+++ CodeWars7kyu.js
@@ -199,3 +199,21 @@ function sumCubes(n){
   }
   return sum
 }
+
+// DESCRIPTION:
+// You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+// lst contains integers, that means it may contain some negative numbers
+// if lst is empty or contains a single element, return 0
+// lst is not sorted
+// [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+// [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+// Have fun!
+
+function maxDiff(list) {
+  list.sort((a,b)=> a -b)
+  if(list == [] || list.length <= 1){
+    return 0
+  }else
+  return list[list.length-1]-list[0]
+};
