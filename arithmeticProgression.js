5c47cb474e52cd2ba739057837ff1a726da06e90
Bimbola Omishore <bomishore85@gmail.com>
2024-04-02 10:24:52 -0400

    codewars 040224

---
+++ arithmeticProgression.js
@@ -0,0 +1,9 @@
+// In your class, you have started lessons about arithmetic progression. Since you are also a programmer, you have decided to write a function that will return the first n elements of the sequence with the given common difference d and first element a. Note that the difference may be zero!
+
+// The result should be a string of numbers, separated by comma and space.
+
+function arithmeticSequenceElements(a,d,n) {
+    var ret = [a]
+    while (--n) ret.push(a+=d);
+    return ret.join(', ')
+  }
