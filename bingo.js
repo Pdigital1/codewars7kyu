fa83728a9aab87cea27187577c4205334bdcd0e4
Bimbola Omishore <bomishore85@gmail.com>
2024-04-09 18:10:17 -0400

    codewars 040924

--- bingo.js
+++ bingo.js
@@ -4,6 +4,10 @@
 
 
 function bingo(a) {
-    return a.includes(21 && 9 && 14) ? "WIN" : "LOSE"
-      
-   }
+    return a.includes(2) &&
+        a.includes(9) &&
+        a.includes(14) &&
+        a.includes(15) &&
+        a.includes(7) ? "WIN" : "LOSE"
+
+}
