6b32ab42be0b8bc6db9e057383896c6e2ae5cf75
Bimbola Omishore <bomishore85@gmail.com>
2024-04-15 11:12:41 -0400

    codewars 041524

---
+++ exclamationMark.js
@@ -0,0 +1,32 @@
+// Description:
+// Count the number of exclamation marks and question marks, return the product.
+
+// Examples
+// ""          --->   0
+// "!"         --->   0
+// "!ab? ?"    --->   2
+// "!!"        --->   0
+// "!??"       --->   2
+// "!???"      --->   3
+// "!!!??"     --->   6
+// "!!!???"    --->   9
+// "!???!!"    --->   9
+// "!????!!!?" --->  20
+
+function product (string) {
+    var wow = 0;
+    var pow = 0;
+    
+    string.split('').forEach(function(pointMark){
+      if(pointMark === '!'){
+        wow +=1
+      }
+      if(pointMark === '?'){
+        pow +=1
+      }
+      if(pointMark === ''){
+        return 0
+      }
+    });
+    return wow * pow
+  }
