52b0cb1015052c4fdcbe55a30a8354c0873b91d2
Bimbola Omishore <bomishore85@gmail.com>
2024-06-11 20:17:40 -0400

    codewars 061124

--- switcheroo.js
+++ switcheroo.js
@@ -7,8 +7,8 @@
 
 // etc
 
-function switcheroo(fsv){
-    return fsv.split('').map(function(e){
+function switcheroo(psv){
+    return psv.split('').map(function(e){
       if (e =='b')return 'a';
       if (e == 'a') return 'b';
       if(e=='c') return 'c'
