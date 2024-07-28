2926b1911ba61722012c33e74a392a09dfde8a2e
Bimbola Omishore <bomishore85@gmail.com>
2024-07-28 11:55:48 -0400

    codewars 072824

--- switcheroo.js
+++ switcheroo.js
@@ -7,8 +7,8 @@
 
 // etc
 
-function switcheroo(sancho){
-    return sancho.split('').map(function(e){
+function switcheroo(hulk){
+    return hulk.split('').map(function(e){
       if (e =='b')return 'a';
       if (e == 'a') return 'b';
       if(e=='c') return 'c'
