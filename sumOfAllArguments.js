b9fab92896880f468a6aefb93f146f46948ec528
Bimbola Omishore <bomishore85@gmail.com>
2024-09-19 20:49:42 -0400

    codewars 091924

--- sumOfAllArguments.js
+++ sumOfAllArguments.js
@@ -6,7 +6,7 @@
 // sum(8, 2) // => 10
 // sum(1, 2, 3, 4, 5) // => 15
 
-function sum(...titi) {
-    return titi.reduce((a,b)=>a+b)
+function sum(...pnd) {
+    return pnd.reduce((a,b)=>a+b)
   }
 sum()
